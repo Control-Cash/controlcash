980350b141017b58c9cde21998333dfffb547107
Adriel <adriel.fsantos@outlook.com>
2023-05-18 16:15:03 -0300

    Adicona rota para detalhar venda #73

--- venda/urls.py
+++ venda/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from venda.views import criar_venda_view, listar_vendas_view
+from venda.views import criar_venda_view, detalhar_venda_view, listar_vendas_view
 
 urlpatterns = [
     path('', listar_vendas_view, name='venda_listar'),
     path('criar', criar_venda_view, name='venda_criar'),
+    path('<int:pk>', detalhar_venda_view, name='venda_detalhar'),
 ]
