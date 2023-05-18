865fdedf52ca93956d01e828b5965906ee76299b
Adriel <adriel.fsantos@outlook.com>
2023-05-18 20:14:49 -0300

    Implementa atualização de estoque ao adicionar item #73 #74

---
+++ produto/signals.py
@@ -0,0 +1,22 @@
+from django.db.models.signals import pre_save, post_save
+from django.dispatch import receiver
+from produto.models import Produto
+
+from venda.models import Item
+
+
+@receiver(pre_save, sender=Item)
+def restaurar_estoque(sender, instance, **kwargs):
+    if instance.id:  # Verifica se o objeto já existe no banco de dados
+        item_anterior = Item.objects.get(id=instance.id)
+
+        produto = Produto.objects.get(id=item_anterior.produto.id)
+        produto.quantidadeEstoque = produto.quantidadeEstoque + item_anterior.quantidade
+        produto.save()
+
+
+@receiver(post_save, sender=Item)
+def atualizar_estoque(sender, instance, **kwargs):
+    produto = Produto.objects.get(id=instance.produto.id)
+    produto.quantidadeEstoque = produto.quantidadeEstoque - instance.quantidade
+    produto.save()
