3c4932f25a2e1b3bbe01431d41425f8a6f33adf5
Adriel <adriel.fsantos@outlook.com>
2023-05-18 18:04:49 -0300

    Implementa view editar_venda_view #73

--- venda/views.py
+++ venda/views.py
@@ -14,10 +14,24 @@ def criar_venda_view(request):
             return redirect('venda_listar')
     return render(request, 'venda/criar_venda.html', {'form': form})
 
+
 def listar_vendas_view(request):
     vendas = Venda.objects.all()
     return render(request, 'venda/listar_vendas.html', {'vendas': vendas})
 
+
 def detalhar_venda_view(request, pk):
     venda = get_object_or_404(Venda, id=pk)
     return render(request, 'venda/detalhar_venda.html', {'venda': venda})
+
+
+def editar_venda_view(request, pk):
+    venda = get_object_or_404(Venda, id=pk)
+    form = VendaForm(instance=venda)
+
+    if request.method == 'POST':
+        form = VendaForm(request.POST, instance=venda)
+        if form.is_valid():
+            form.save()
+            return redirect(request, 'detalhar_produto', {'pk': pk})
+    return render(request, 'venda/editar_venda.html', {'form': form})
