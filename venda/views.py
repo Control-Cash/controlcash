4cb57f90292faeebf9345d9fff35906fd75d6bde
Adriel <adriel.fsantos@outlook.com>
2023-05-18 15:58:37 -0300

    Implementa view listar_vendas_view #73

--- venda/views.py
+++ venda/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from venda.models import Venda
+
+
+def listar_vendas_view(request):
+    vendas = Venda.objects.all()
+    return render(request, 'venda/listar_vendas.html', {'vendas': vendas})
