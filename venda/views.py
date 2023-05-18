e2806a4913f507f4c54dda3f485e37bfef595d55
Adriel <adriel.fsantos@outlook.com>
2023-05-18 18:26:23 -0300

    Renomeia templates da pasta venda #73

--- venda/views.py
+++ venda/views.py
@@ -12,17 +12,17 @@ def criar_venda_view(request):
         if form.is_valid():
             form.save()
             return redirect('venda_listar')
-    return render(request, 'venda/criar_venda.html', {'form': form})
+    return render(request, 'venda/criar.html', {'form': form})
 
 
 def listar_vendas_view(request):
     vendas = Venda.objects.all()
-    return render(request, 'venda/listar_vendas.html', {'vendas': vendas})
+    return render(request, 'venda/listar.html', {'vendas': vendas})
 
 
 def detalhar_venda_view(request, pk):
     venda = get_object_or_404(Venda, id=pk)
-    return render(request, 'venda/detalhar_venda.html', {'venda': venda})
+    return render(request, 'venda/detalhar.html', {'venda': venda})
 
 
 def editar_venda_view(request, pk):
@@ -34,7 +34,7 @@ def editar_venda_view(request, pk):
         if form.is_valid():
             form.save()
             return redirect('detalhar_produto', pk=pk)
-    return render(request, 'venda/editar_venda.html', {'form': form})
+    return render(request, 'venda/editar.html', {'form': form})
 
 
 def desativar_venda_view(request, pk):
@@ -43,4 +43,4 @@ def desativar_venda_view(request, pk):
         venda.status = False
         venda.save()
         return redirect('venda_listar', pk=pk)
-    return render(request, 'venda/desativar_venda.html', {'venda': venda})
+    return render(request, 'venda/desativar.html', {'venda': venda})
