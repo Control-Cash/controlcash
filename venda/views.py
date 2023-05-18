45f11f9a41f3945ef56d954864210cb0c64c4abd
Adriel <adriel.fsantos@outlook.com>
2023-05-18 15:58:57 -0300

    Implementa view criar_venda_view #73

--- venda/views.py
+++ venda/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from venda.forms import VendaForm
 from venda.models import Venda
 
+
+def criar_venda_view(request):
+    form = VendaForm()
+
+    if request.method == 'POST':
+        form = VendaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('venda_listar')
+    return render(request, 'venda/criar_venda.html', {'form': form})
 
 def listar_vendas_view(request):
     vendas = Venda.objects.all()
