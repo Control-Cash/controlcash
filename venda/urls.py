fbf9ba3998535f30cca20e5d3537437c76be99a8
Adriel <adriel.fsantos@outlook.com>
2023-05-18 18:05:02 -0300

    Adicona rota para editar venda #73

--- venda/urls.py
+++ venda/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from venda.views import criar_venda_view, detalhar_venda_view, listar_vendas_view
+from venda.views import (criar_venda_view, detalhar_venda_view,
+                         editar_venda_view, listar_vendas_view)
 
 urlpatterns = [
     path('', listar_vendas_view, name='venda_listar'),
     path('criar', criar_venda_view, name='venda_criar'),
     path('<int:pk>', detalhar_venda_view, name='venda_detalhar'),
+    path('<int:pk>/editar', editar_venda_view, name='venda_editar'),
 ]
