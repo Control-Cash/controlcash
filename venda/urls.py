acf5991f2053fc310141dc71a1136c818fc5ebd3
Adriel <adriel.fsantos@outlook.com>
2023-05-18 18:23:05 -0300

    Adicona rota para desativar venda #73

--- venda/urls.py
+++ venda/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from venda.views import (criar_venda_view, detalhar_venda_view,
-                         editar_venda_view, listar_vendas_view)
+from venda.views import (criar_venda_view, desativar_venda_view,
+                         detalhar_venda_view, editar_venda_view,
+                         listar_vendas_view)
 
 urlpatterns = [
     path('', listar_vendas_view, name='venda_listar'),
     path('criar', criar_venda_view, name='venda_criar'),
     path('<int:pk>', detalhar_venda_view, name='venda_detalhar'),
     path('<int:pk>/editar', editar_venda_view, name='venda_editar'),
+    path('<int:pk>/desativar', desativar_venda_view, name='venda_desativar'),
 ]
