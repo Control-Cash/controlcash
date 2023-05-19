5610434ae1f648d82e94f9577a844ddd066d5011
Adriel <adriel.fsantos@outlook.com>
2023-05-19 16:25:06 -0300

    Divide em views em arquivos por modelo #73 #74 #76

--- venda/urls.py
+++ venda/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from venda.views import (criar_venda_view, desativar_venda_view,
-                         detalhar_venda_view, editar_quatidade_item_view,
-                         finalizar_venda_view, listar_vendas_view, reativar_venda_view,
-                         remover_item_view)
+from venda.views.item_views import (editar_quatidade_item_view,
+                                    remover_item_view)
+from venda.views.venda_views import (criar_venda_view, desativar_venda_view,
+                                     detalhar_venda_view, finalizar_venda_view,
+                                     listar_vendas_view, reativar_venda_view)
 
 urlpatterns = [
     path('', listar_vendas_view, name='venda_listar'),
--- venda/views/venda_views.py
+++ venda/views/venda_views.py
@@ -1,6 +1,7 @@
+
 from django.shortcuts import get_object_or_404, redirect, render
 
-from venda.forms import CriarVendaForm, EditarItemVendaForm, ItemVendaForm
+from venda.forms import CriarVendaForm, ItemVendaForm
 from venda.models import Item, Venda
 
 
@@ -81,24 +82,3 @@ def reativar_venda_view(request, pk):
         venda.save()
         return redirect('venda_detalhar', pk=pk)
     return render(request, 'venda/venda/reativar.html', {'venda': venda})
-
-
-def editar_quatidade_item_view(request, pk):
-    item = get_object_or_404(Item, id=pk)
-    form = EditarItemVendaForm(instance=item)
-
-    if request.method == 'POST':
-        form = EditarItemVendaForm(request.POST, instance=item)
-        if form.is_valid():
-            form.save()
-            return redirect('venda_detalhar', pk=item.venda.id)
-    return render(request, 'venda/item/editar.html', {'form': form, 'item': item})
-
-
-def remover_item_view(request, pk):
-    item = get_object_or_404(Item, id=pk)
-    venda_id = item.venda.id
-    if request.method == 'POST':
-        item.delete()
-        return redirect('venda_detalhar', pk=venda_id)
-    return render(request, 'venda/item/remover.html', {'item': item})
