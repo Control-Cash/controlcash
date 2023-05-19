baf5492d68d9cc5fc0ebc064f0b9ee19bf00849b
Adriel <adriel.fsantos@outlook.com>
2023-05-19 17:03:15 -0300

    Implementa listar_clientes_view #76

--- venda/views/cliente_views.py
+++ venda/views/cliente_views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import redirect, render
+
 from venda.forms import ClienteForm
+from venda.models import Cliente
 
 
 def criar_cliente_view(request):
@@ -12,3 +14,8 @@ def criar_cliente_view(request):
             form.save()
             return redirect('venda_listar')
     return render(request, 'venda/cliente/criar.html', {'form': form})
+
+
+def listar_clientes_view(request):
+    clientes = Cliente.objects.all()
+    return render(request, 'venda/cliente/listar.html', {'clientes': clientes})
