271f9fe2e708e789e38298cfd62381f6b848fe5e
Adriel <adriel.fsantos@outlook.com>
2023-05-18 22:22:58 -0300

    Implementa rota de reativação de venda #73

--- venda/urls.py
+++ venda/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from venda.views import (criar_venda_view, desativar_venda_view,
                          detalhar_venda_view, editar_quatidade_item_view,
-                         finalizar_venda_view, listar_vendas_view,
+                         finalizar_venda_view, listar_vendas_view, reativar_venda_view,
                          remover_item_view)
 
 urlpatterns = [
@@ -11,6 +11,7 @@ urlpatterns = [
     path('<int:pk>', detalhar_venda_view, name='venda_detalhar'),
     path('<int:pk>/desativar', desativar_venda_view, name='venda_desativar'),
     path('<int:pk>/finalizar', finalizar_venda_view, name='venda_finalizar'),
+    path('<int:pk>/reativar', reativar_venda_view, name='venda_reativar'),
     path('itens/<int:pk>/editar', editar_quatidade_item_view, name='item_editar'),
     path('itens/<int:pk>/remover', remover_item_view, name='item_remover'),
 ]
