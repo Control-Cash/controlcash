d80a766801ad3a1a8941678ae7cce78bf57acced
Adriel <adriel.fsantos@outlook.com>
2023-05-18 21:30:51 -0300

    Adiciona rota para edição de item de venda #74

--- venda/urls.py
+++ venda/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from venda.views import (criar_venda_view, desativar_venda_view,
-                         detalhar_venda_view, listar_vendas_view)
+                         detalhar_venda_view, editar_quatidade_item_view, listar_vendas_view)
 
 urlpatterns = [
     path('', listar_vendas_view, name='venda_listar'),
     path('criar', criar_venda_view, name='venda_criar'),
     path('<int:pk>', detalhar_venda_view, name='venda_detalhar'),
     path('<int:pk>/desativar', desativar_venda_view, name='venda_desativar'),
+    path('itens/<int:pk>/editar', editar_quatidade_item_view, name='item_editar')
 ]
