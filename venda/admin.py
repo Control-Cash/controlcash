c3f60cee6a0ea9ce6e3ba385a52376d3c245365d
Adriel <adriel.fsantos@outlook.com>
2023-05-19 16:10:58 -0300

    Insere modelo Cliente a pagina administrativa #76

--- venda/admin.py
+++ venda/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from venda.models import Item, Venda
+from venda.models import Cliente, Item, Venda
 
 admin.site.register(Venda)
 admin.site.register(Item)
+admin.site.register(Cliente)
