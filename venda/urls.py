d1eaaf0d0d0a50073927a9e9e95125722222b144
Adriel <adriel.fsantos@outlook.com>
2023-05-18 21:55:33 -0300

    Adiciona rota para finalização de venda #73

--- venda/urls.py
+++ venda/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 
 from venda.views import (criar_venda_view, desativar_venda_view,
-                         detalhar_venda_view, editar_quatidade_item_view, listar_vendas_view)
+                         detalhar_venda_view, editar_quatidade_item_view,
+                         finalizar_venda_view, listar_vendas_view)
 
 urlpatterns = [
     path('', listar_vendas_view, name='venda_listar'),
     path('criar', criar_venda_view, name='venda_criar'),
     path('<int:pk>', detalhar_venda_view, name='venda_detalhar'),
     path('<int:pk>/desativar', desativar_venda_view, name='venda_desativar'),
+    path('<int:pk>/finalizar', finalizar_venda_view, name='venda_finalizar'),
     path('itens/<int:pk>/editar', editar_quatidade_item_view, name='item_editar')
 ]
