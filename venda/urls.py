94cf054f8333e07a464ba7168fd76140705cad9c
Adriel <adriel.fsantos@outlook.com>
2023-05-18 22:18:21 -0300

    Adiciona rota para remover item #74

--- venda/urls.py
+++ venda/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from venda.views import (criar_venda_view, desativar_venda_view,
                          detalhar_venda_view, editar_quatidade_item_view,
-                         finalizar_venda_view, listar_vendas_view)
+                         finalizar_venda_view, listar_vendas_view,
+                         remover_item_view)
 
 urlpatterns = [
     path('', listar_vendas_view, name='venda_listar'),
@@ -10,5 +11,6 @@ urlpatterns = [
     path('<int:pk>', detalhar_venda_view, name='venda_detalhar'),
     path('<int:pk>/desativar', desativar_venda_view, name='venda_desativar'),
     path('<int:pk>/finalizar', finalizar_venda_view, name='venda_finalizar'),
-    path('itens/<int:pk>/editar', editar_quatidade_item_view, name='item_editar')
+    path('itens/<int:pk>/editar', editar_quatidade_item_view, name='item_editar'),
+    path('itens/<int:pk>/remover', remover_item_view, name='item_remover'),
 ]
