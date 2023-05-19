6274b787b0115de14ce7c2ea2afa5efa77dba647
Adriel <adriel.fsantos@outlook.com>
2023-05-19 17:25:07 -0300

    Implementa editar_cliente_view #76

--- venda/views/cliente_views.py
+++ venda/views/cliente_views.py
@@ -24,3 +24,16 @@ def listar_clientes_view(request):
 def detalhar_cliente_view(request, pk):
     cliente = get_object_or_404(Cliente, id=pk)
     return render(request, 'venda/cliente/detalhar.html', {'cliente': cliente})
+
+
+def editar_cliente_view(request, pk):
+    cliente = get_object_or_404(Cliente, id=pk)
+    form = ClienteForm(instance=cliente)
+
+    if request.method == 'POST':
+        form = ClienteForm(request.POST, instance=cliente)
+
+        if form.is_valid():
+            form.save()
+        return redirect('cliente_detalhar', pk=pk)
+    return render(request, 'venda/cliente/editar.html', {'form': form})
