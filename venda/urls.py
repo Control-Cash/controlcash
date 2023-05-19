d7529a494e7b592ae86fa5ad2633a4b9689c9e14
Adriel <adriel.fsantos@outlook.com>
2023-05-19 16:51:27 -0300

    Adiciona rota para criação de cliente #76

--- venda/urls.py
+++ venda/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
+from venda.views.cliente_views import criar_cliente_view
 from venda.views.item_views import (editar_quatidade_item_view,
                                     remover_item_view)
 from venda.views.venda_views import (criar_venda_view, desativar_venda_view,
@@ -7,12 +8,16 @@ from venda.views.venda_views import (criar_venda_view, desativar_venda_view,
                                      listar_vendas_view, reativar_venda_view)
 
 urlpatterns = [
+    # relacionadas ao modelo venda
     path('', listar_vendas_view, name='venda_listar'),
     path('criar', criar_venda_view, name='venda_criar'),
     path('<int:pk>', detalhar_venda_view, name='venda_detalhar'),
     path('<int:pk>/desativar', desativar_venda_view, name='venda_desativar'),
     path('<int:pk>/finalizar', finalizar_venda_view, name='venda_finalizar'),
     path('<int:pk>/reativar', reativar_venda_view, name='venda_reativar'),
+    # relacionadas ao modelo item
     path('itens/<int:pk>/editar', editar_quatidade_item_view, name='item_editar'),
     path('itens/<int:pk>/remover', remover_item_view, name='item_remover'),
+    # relacionadas ao modelo cliente
+    path('cliente/criar', criar_cliente_view, name="cliente_criar"),
 ]
