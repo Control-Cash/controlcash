23a32c9caf402bc670ab5bd622fa3d3adb43ca42
Adriel <adriel.fsantos@outlook.com>
2023-05-20 17:05:04 -0300

    Implementa test_model_string_representation_is_venda_details #73

--- venda/tests/venda/test_models.py
+++ venda/tests/venda/test_models.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from venda.models import Cliente
+from venda.models import Cliente, Venda
 
 
 class ClienteModel(TestCase):
@@ -18,4 +18,23 @@ class ClienteModel(TestCase):
             str(self.cliente),
             f"{self.cliente.nome}",
             "A representação string do modelo 'Cliente' não é o seu atributo nome"
+        )
+
+
+class VendaModel(TestCase):
+    def setUp(self) -> None:
+        self.cliente = Cliente.objects.create(
+            nome='João',
+            email="joao@gmail.com"
+        )
+        self.venda = Venda.objects.create(cliente=self.cliente)
+
+    def test_model_string_representation_is_venda_details(self):
+        """Verifica se a representação string do modelo 'Venda' é uma string com
+        detalhes a seu respeito"""
+
+        self.assertEqual(
+            str(self.venda),
+            f"{self.venda.item_set.count} itens vendidos para {self.venda.cliente} ({self.venda.status})",
+            "A representação string do modelo 'Venda' é diferente do esperado"
         )
