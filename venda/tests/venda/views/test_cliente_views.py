ef5ba1f7797ab114c4929353e0f5eb3d767d82f2
Adriel <adriel.fsantos@outlook.com>
2023-05-20 15:50:55 -0300

    Corrige erros de digitação e formatação de testes #76

--- venda/tests/venda/views/test_cliente_views.py
+++ venda/tests/venda/views/test_cliente_views.py
@@ -18,6 +18,7 @@ class CriarClienteView(TestCase):
 
     def test_view_sends_form_to_template(self):
         """Verifica se a view envia o formulário ao template"""
+
         response = self.client.get(self.target_url)
         self.assertIsNotNone(
             response.context.get('form'),
@@ -25,38 +26,52 @@ class CriarClienteView(TestCase):
 
     def test_form_sended_to_template_is_cliente_form(self):
         """Verifica se a view envia o formulário correto ao template"""
+
         response = self.client.get(self.target_url)
+
         self.assertIsInstance(
             response.context.get('form'),
             ClienteForm,
             "'form' enviado pela view não é 'ClienteForm'")
 
     def test_empty_form_doesnt_save(self):
-        """Verifica se a view nao cria um cliente quando um post request tem os
+        """Verifica se a view não cria um cliente quando um post request tem os
         dados em branco"""
+
         cliente_count = Cliente.objects.count()
         self.client.post(self.target_url, {
             'nome': '',
             'email': ''
         })
-        self.assertEqual(cliente_count, Cliente.objects.count(
-        ), "Um cliente foi criado com o formulário em branco")
+
+        self.assertEqual(
+            cliente_count,
+            Cliente.objects.count(),
+            "Um cliente foi criado com o formulário em branco"
+        )
 
     def test_empty_form_return_errors(self):
         """Verifica se a view envia erros ao template quando recebe dados em
         branco"""
-        response = self.client.post(self.target_url, {
-            'nome': '',
-            'email': ''
-        })
+
+        response = self.client.post(
+            self.target_url,
+            {
+                'nome': '',
+                'email': ''
+            }
+        )
         form = response.context.get('form')
+
         self.assertIsNotNone(form.errors)
 
     def test_filled_form_saves(self):
         """Verifica se a view cria um cliente quando um post request tem os
         dados corretos"""
+
         cliente_count = Cliente.objects.count()
         self.client.post(self.target_url, self.form_data)
+
         self.assertEqual(
             cliente_count + 1,
             Cliente.objects.count(),
@@ -65,11 +80,16 @@ class CriarClienteView(TestCase):
     def test_empty_required_fields_doesnt_save(self):
         """Verifica se a view nao cria um cliente quando um post request tem os
         dados obrigatótios em branco"""
+
         cliente_count = Cliente.objects.count()
-        self.client.post(self.target_url, {
-            'nome': '',
-            'email': self.form_data.get('email')
-        })
+        self.client.post(
+            self.target_url,
+            {
+                'nome': '',
+                'email': self.form_data.get('email')
+            }
+        )
+
         self.assertEqual(
             cliente_count,
             Cliente.objects.count(),
@@ -78,42 +98,59 @@ class CriarClienteView(TestCase):
     def test_empty_optional_fields_saves(self):
         """Verifica se a view cria um cliente quando um post request tem os
         dados opcionais em branco"""
+
         cliente_count = Cliente.objects.count()
-        self.client.post(self.target_url, {
-            'nome': self.form_data.get('nome'),
-            'email': ''
-        })
+        self.client.post(
+            self.target_url,
+            {
+                'nome': self.form_data.get('nome'),
+                'email': ''
+            }
+        )
+
         self.assertEqual(
             cliente_count + 1,
             Cliente.objects.count(),
-            "Um cliente não foi criado com os campos opcionais do formulário em branco")
+            "Um cliente não foi criado com os campos opcionais do formulário em branco"
+        )
 
     def test_empty_required_fields_return_errors(self):
         """Verifica se a view envia erros ao template quando recebe todos os 
         dados obrigatórios em branco"""
-        response = self.client.post(self.target_url, {
-            'nome': '',
-            'email': self.form_data.get('email')
-        })
+
+        response = self.client.post(
+            self.target_url,
+            {
+                'nome': '',
+                'email': self.form_data.get('email')
+            }
+        )
         form = response.context.get('form')
+
         self.assertIsNotNone(form.errors)
 
     def test_view_uses_correct_template(self):
         """Verifica se a view usa o template correto na renderizacao"""
+
         response = self.client.get(self.target_url)
+
         self.assertTemplateUsed(
             response,
             self.expeted_template,
-            'A view usou um template diferente do esperado')
+            'A view usou um template diferente do esperado'
+        )
 
     def test_successful_post_request_redirects(self):
         """Verifica se a view redireciona quando uma solicitação post é enviada
         corretamente com todos os dados"""
+
         response = self.client.post(self.target_url, self.form_data)
+
         self.assertEqual(
             response.status_code,
             302,
-            'A view não redirecionou após post request bem sucedido')
+            'A view não redirecionou após post request bem sucedido'
+        )
 
     def test_successful_post_request_redirects_to_expected_url(self):
         """Verifica se a view redireciona para a url correta quando uma
@@ -132,7 +169,8 @@ class CriarClienteView(TestCase):
                 }
             ),
             302,
-            200)
+            200
+        )
 
 
 class ListarClientesView(TestCase):
@@ -160,9 +198,11 @@ class ListarClientesView(TestCase):
         """Verifica se a view envia uma variavel 'clientes' ao template"""
 
         response = self.client.get(self.target_url)
+
         self.assertIsNotNone(
             response.context.get('clientes'),
-            "'clientes' não é enviado pela view")
+            "'clientes' não é enviado pela view"
+        )
 
     def test_clientes_sended_to_template_contains_clientes_data(self):
         """Verifica se a view envia os dados dos clientes ao template"""
@@ -178,8 +218,8 @@ class ListarClientesView(TestCase):
         self.assertIsInstance(
             clientes,
             QuerySet,
-            "'clientes' enviado pela view não é conjunto de modelos")
-
+            "'clientes' enviado pela view não é conjunto de modelos"
+        )
         self.assertTrue(
             all(isinstance(cliente, Cliente) for cliente in clientes),
             "Os objetos retornados não são do modelo 'Cliente'"
@@ -187,8 +227,11 @@ class ListarClientesView(TestCase):
 
     def test_view_uses_correct_template(self):
         """Verifica se a view usa o template correto na renderização"""
+
         response = self.client.get(self.target_url)
+
         self.assertTemplateUsed(
             response,
             self.expeted_template,
-            'A view usou um template diferente do esperado')
+            'A view usou um template diferente do esperado'
+        )
