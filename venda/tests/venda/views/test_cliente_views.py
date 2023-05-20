3e04fc51968cb9ea46b9a955f814e90ddd8d55d6
Adriel <adriel.fsantos@outlook.com>
2023-05-20 12:55:38 -0300

    Implementa test_form_sended_to_template_is_cliente_form #76

--- venda/tests/venda/views/test_cliente_views.py
+++ venda/tests/venda/views/test_cliente_views.py
@@ -1,6 +1,8 @@
 from django.test import Client, TestCase
 from django.urls import reverse_lazy
 
+from venda.forms import ClienteForm
+
 
 class CriarClienteView(TestCase):
     def setUp(self) -> None:
@@ -13,3 +15,11 @@ class CriarClienteView(TestCase):
         self.assertIsNotNone(
             response.context.get('form'),
             "'form' não é enviado pela view")
+
+    def test_form_sended_to_template_is_cliente_form(self):
+        """Verifica se a view envia o formulário correto ao template"""
+        response = self.client.get(self.target_url)
+        self.assertIsInstance(
+            response.context.get('form'),
+            ClienteForm,
+            "'form' enviado pela view não é 'ClienteForm'")
