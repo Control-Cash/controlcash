681f45c4bfaaee0b1c25f9d02766b76d470f8406
Adriel <adriel.fsantos@outlook.com>
2023-05-20 12:58:38 -0300

    Implementa test_empty_form_doesnt_save #76

--- venda/tests/venda/views/test_cliente_views.py
+++ venda/tests/venda/views/test_cliente_views.py
@@ -2,12 +2,17 @@ from django.test import Client, TestCase
 from django.urls import reverse_lazy
 
 from venda.forms import ClienteForm
+from venda.models import Cliente
 
 
 class CriarClienteView(TestCase):
     def setUp(self) -> None:
         self.client = Client()
         self.target_url = reverse_lazy('venda:cliente_criar')
+        self.form_data = {
+            'nome': 'José Silva',
+            'email': 'josesilva@gmail.com'
+        }
 
     def test_view_sends_form_to_template(self):
         """Verifica se a view envia o formulário ao template"""
@@ -23,3 +28,14 @@ class CriarClienteView(TestCase):
             response.context.get('form'),
             ClienteForm,
             "'form' enviado pela view não é 'ClienteForm'")
+
+    def test_empty_form_doesnt_save(self):
+        """Verifica se a view nao cria um cliente quando um post request tem os
+        dados em branco"""
+        cliente_count = Cliente.objects.count()
+        self.client.post(self.target_url, {
+            'nome': '',
+            'email': ''
+        })
+        self.assertEqual(cliente_count, Cliente.objects.count(
+        ), "Um cliente foi criado com o formulário em branco")
