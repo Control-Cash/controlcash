8567d99324a1ecc45b304b3cc6c118a95c818815
Adriel <adriel.fsantos@outlook.com>
2023-05-21 14:43:55 -0300

    Implementa test_correct_form_in_context #73

--- venda/tests/venda/views/test_venda_views.py
+++ venda/tests/venda/views/test_venda_views.py
@@ -1,7 +1,11 @@
+from datetime import date
+from decimal import Decimal
 from django.test import Client, TestCase
 from django.urls import reverse_lazy
+from venda.forms import CriarVendaForm
 
-from venda.models import Cliente, Venda
+from venda.models import Cliente, Item, Venda
+from produto.models import Produto
 
 
 class ListarVendasView(TestCase):
@@ -37,3 +41,39 @@ class ListarVendasView(TestCase):
         self.assertIsInstance(self.vendas[0], Venda)
         self.assertIsInstance(self.vendas[1], Venda)
         self.assertIsInstance(self.vendas[2], Venda)
+
+
+class CriarVendaView(TestCase):
+    def setUp(self) -> None:
+        self.target_url = reverse_lazy('venda:venda_criar')
+        self.expected_template = 'venda/venda/criar.html'
+        self.expected_form_class = CriarVendaForm
+        self.expected_url_redirect_name = 'venda:venda_detalhar'
+        self.client = Client()
+        self.cliente = Cliente.objects.create(nome='João Silva')
+        self.produto = Produto.objects.create(
+            nome='Celular',
+            precoVenda=Decimal(1000),
+            quantidadeEstoque=10,
+            dataRegistro=date(2023, 2, 10)
+        )
+
+    def test_correct_form_in_context(self):
+        """Verifica se a view envia um formulário para o template e se esse
+        formulário é da classe correta"""
+
+        response = self.client.get(self.target_url)
+        form = response.context.get('form')
+
+        self.assertIsNotNone(form)
+        self.assertIsInstance(form, self.expected_form_class)
+
+    # template correto
+    # nao cria venda sem cliente
+    # nao cria venda sem item
+    # redireciona para a url correta ao salvar
+    # cria venda quando os dados tao corretos
+    # retorna erros no form quando vai preenchido errado
+    # nao cria venda quando a quantidade do item é maior que o estoque de produto
+    # form inicial carrega com os campos vazios
+    # form apos erro de validacao renderiza com os campos preenchidos anteriormente
