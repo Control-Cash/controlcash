315ae387c3a78d7bce45b11198370a0b2ef2f3fa
GuilhermeAngelo <guilhermeangelo2001@gmail.com>
2023-05-25 09:59:35 -0300

    renomeando os campos do model Porduto #71

--- produto/models.py
+++ produto/models.py
@@ -9,10 +9,10 @@ class Produto(models.Model):
     # Campos do modelo
 
     nome = models.CharField(max_length=150)
-    precoVenda = models.DecimalField(max_digits=10,decimal_places=2, verbose_name="Preço Venda", validators=[MinValueValidator(0.00)])
+    preco_venda = models.DecimalField(max_digits=10,decimal_places=2, verbose_name="Preço Venda", validators=[MinValueValidator(0.00)])
     descricao = models.TextField(blank=True, null=True, verbose_name="Descrição")
-    quantidadeEstoque = models.PositiveIntegerField(verbose_name="Quatidade")
-    dataRegistro = models.DateField(auto_now_add = True, verbose_name="Data")
+    quantidade_estoque = models.PositiveIntegerField(verbose_name="Quatidade")
+    data_registro = models.DateField(auto_now_add = True, verbose_name="Data")
 
     def __str__(self):
         return self.nome
