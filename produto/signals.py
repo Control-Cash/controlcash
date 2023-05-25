6fc8654ccb5f6e6baa57687dfa10784231e98c64
Adriel <adriel.fsantos@outlook.com>
2023-05-25 18:23:08 -0300

    Corrige nome de atributos do modelo Produto #85

--- produto/signals.py
+++ produto/signals.py
@@ -6,24 +6,24 @@ from venda.models import Item
 
 
 @receiver(pre_save, sender=Item)
-def restaurar_estoque_antes_de_atualizar_item(sender, instance, **kwargs):
+def restaurar_estoque_antes_de_atualizar_item(sender, instance: Item, **kwargs):
     if instance.id:  # Verifica se o objeto já existe no banco de dados
         item_anterior = Item.objects.get(id=instance.id)
 
         produto = Produto.objects.get(id=item_anterior.produto.id)
-        produto.quantidadeEstoque = produto.quantidadeEstoque + item_anterior.quantidade
+        produto.quantidade_estoque = produto.quantidade_estoque + item_anterior.quantidade
         produto.save()
 
 
 @receiver(post_save, sender=Item)
-def atualizar_estoque_apos_criar_ou_atualizar_item(sender, instance, **kwargs):
+def atualizar_estoque_apos_criar_ou_atualizar_item(sender, instance: Item, **kwargs):
     produto = Produto.objects.get(id=instance.produto.id)
-    produto.quantidadeEstoque = produto.quantidadeEstoque - instance.quantidade
+    produto.quantidade_estoque = produto.quantidade_estoque - instance.quantidade
     produto.save()
 
 
 @receiver(pre_delete, sender=Item)
-def restaurar_estoque_antes_de_remover_item(sender, instance, **kwargs):
+def restaurar_estoque_antes_de_remover_item(sender, instance: Item, **kwargs):
     produto = Produto.objects.get(id=instance.produto.id)
-    produto.quantidadeEstoque = produto.quantidadeEstoque + instance.quantidade
+    produto.quantidade_estoque= produto.quantidade_estoque + instance.quantidade
     produto.save()
