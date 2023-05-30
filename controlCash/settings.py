ca2c3e755dcaef11ce42ddc864f3c0924ce50b4b
hilariodantas <hilariod94@gmail.com>
2023-05-30 09:44:49 -0300

    update #75

--- controlCash/settings.py
+++ controlCash/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'produto.apps.ProdutoConfig',
     'venda.apps.VendaConfig',
+    'pagamento.apps.PagamentoConfig',
 ]
 
 MIDDLEWARE = [
--- pagamento/views.py
+++ pagamento/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home_pagamento(request):
+    return render(request, 'lista_pagamento.html')
