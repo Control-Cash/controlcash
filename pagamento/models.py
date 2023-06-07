ab4302b6a4904a0d5b9daf1da577b027cebe30e0
hilariodantas <hilariod94@gmail.com>
2023-06-07 19:56:18 -0300

    ajustando models.py #75

--- pagamento/models.py
+++ pagamento/models.py
@@ -16,17 +16,15 @@ class FormaPagamento(models.Model):
     
 # Modelo Pagamento
 class Pagamento(models.Model):
-    PENDENTE = 'P'
-    RECEBIDO = 'R'
-    CANCELADO = 'C'
     STATUS_CHOICES = [
-        (PENDENTE, 'Pendente'),
-        (RECEBIDO, 'Recebido'),
-        (CANCELADO, 'Cancelado'),
+        ('pendente', 'Pendente'),
+        ('recebido', 'Recebido'),
+        ('cacelado', 'Cancelado'),
     ]
+
     valor = models.DecimalField(max_digits=10, decimal_places=2)
     data = models.DateField()
-    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default=PENDENTE)
+    status = models.CharField(max_length=8, choices=STATUS_CHOICES, default='pendente')
     id_cliente = models.IntegerField()
 
     def __str__(self):
