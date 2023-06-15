10560cce0f2906535aae01f3268ffdbc464f6ee5
Adriel <adriel.fsantos@outlook.com>
2023-06-15 20:48:43 -0300

    Adiciona rota para cadastro de despesas #105

--- despesa/urls.py
+++ despesa/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from despesa.views import listar_despesas_view
+from despesa.views import criar_despesa_view, listar_despesas_view
 
 app_name = 'despesa'
 
 urlpatterns = [
     path('', listar_despesas_view, name='despesa_listar'),
+    path('criar', criar_despesa_view, name='despesa_criar'),
 ]
