ead5758ae76044d71027e996244286eef33be7a3
Adriel <adriel.fsantos@outlook.com>
2023-06-15 16:34:26 -0300

    Adiciona listar_despesas_view #105

--- despesa/views.py
+++ despesa/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from despesa.models import Despesa
+
+
+def listar_despesas_view(request):
+    despesas = Despesa.objects.all()
+    return render(request, 'despesa/listar.html', {"despesas": despesas})
