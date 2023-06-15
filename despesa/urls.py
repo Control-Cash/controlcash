23d5c81416ffde5c42e2a44895e08f0c2f8f2a0a
Adriel <adriel.fsantos@outlook.com>
2023-06-15 20:59:44 -0300

    Adiciona rota para remoção de despesas #105

--- despesa/urls.py
+++ despesa/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from despesa.views import (criar_despesa_view, editar_despesa_view,
-                           listar_despesas_view)
+                           listar_despesas_view, remover_despesa_view)
 
 app_name = 'despesa'
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('', listar_despesas_view, name='despesa_listar'),
     path('criar', criar_despesa_view, name='despesa_criar'),
     path('editar/<int:pk>', editar_despesa_view, name='despesa_editar'),
+    path('remover/<int:pk>', remover_despesa_view, name='despesa_remover'),
 ]
