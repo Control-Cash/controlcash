0c2d0792e003d7be9e7df446639047eccaade9cc
Adriel <adriel.fsantos@outlook.com>
2023-06-15 16:23:55 -0300

    Adiciona modelo despesa #105

--- despesa/models.py
+++ despesa/models.py
@@ -1,3 +1,12 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Despesa(models.Model):
+    nome = models.CharField(max_length=150)
+    valor = models.DecimalField(max_digits=10, decimal_places=2, validators=[
+                                MinValueValidator(0.01)])
+    periodica = models.BooleanField(verbose_name="periódica", default=False)
+
+    def __str__(self) -> str:
+        return f"{self.nome} ({self.valor})"
