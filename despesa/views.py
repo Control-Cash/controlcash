3557777821aacfd1718acc44d5085d16c440632f
Adriel <adriel.fsantos@outlook.com>
2023-06-15 20:48:34 -0300

    Adiciona criar_despesa_view #105

--- despesa/views.py
+++ despesa/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from despesa.forms import DespesaForm
 
 from despesa.models import Despesa
 
@@ -6,3 +7,14 @@ from despesa.models import Despesa
 def listar_despesas_view(request):
     despesas = Despesa.objects.all()
     return render(request, 'despesa/listar.html', {"despesas": despesas})
+
+
+def criar_despesa_view(request):
+    form = DespesaForm()
+
+    if request.method == 'POST':
+        form = DespesaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('despesa:despesa_listar')
+    return render(request, 'despesa/criar.html', {'form': form})
