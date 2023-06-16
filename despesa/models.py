cb2898bc89cca7006b348c721a7f5bf5c98bb3fc
Adriel <adriel.fsantos@outlook.com>
2023-06-15 22:01:54 -0300

    Corrige validador do campo valor #105

--- despesa/models.py
+++ despesa/models.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 from django.core.validators import MinValueValidator
 from django.db import models
 
@@ -5,7 +7,7 @@ from django.db import models
 class Despesa(models.Model):
     nome = models.CharField(max_length=150)
     valor = models.DecimalField(max_digits=10, decimal_places=2, validators=[
-                                MinValueValidator(0.01)])
+                                MinValueValidator(Decimal('0.01'))])
     periodica = models.BooleanField(verbose_name="periódica", default=False)
 
     def __str__(self) -> str:
