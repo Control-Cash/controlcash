4cbc364f9998b58087b0a06af88e247682c57c46
Adriel <adriel.fsantos@outlook.com>
2023-06-22 23:12:14 -0300

    Configura conexão com banco de dados mariadb #107

--- controlCash/settings.py
+++ controlCash/settings.py
@@ -77,9 +77,17 @@ WSGI_APPLICATION = 'controlCash.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'controlcash',
+        'USER': 'controlcash',
+        'PASSWORD': 'controlcash',
+        'HOST': '127.0.0.1',
+        'PORT': 3306
     }
 }
 
