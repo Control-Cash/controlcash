c23441914600d7114b771b118464b0f420b5e16e
hilariodantas <hilariod94@gmail.com>
2023-06-26 17:06:25 -0300

    desenvolvendo listar pagamento #75

--- pagamento/urls.py
+++ pagamento/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from pagamento.views import home_pagamento
+from pagamento.views import home_pagamento_view, pagamento_criar_view
+
+app_name ='pagamento'
 
 urlpatterns = [
-    path('', home_pagamento, name='home_pagamento'),
+    path('', home_pagamento_view, name='home_pagamento'),
+    path('criar', pagamento_criar_view, name='pagamento_criar')
 ]
--- pagamento/views.py
+++ pagamento/views.py
@@ -1,11 +1,30 @@
 from django.shortcuts import get_object_or_404, render, redirect
+
 from pagamento.models import FormaPagamento, Pagamento
 from pagamento.forms import FormaPagamentoForm, PagamentoForm
 
 redirect_response = '/pagamento'
 
-def home_pagamento(request):
+
+def home_pagamento_view(request):
+    pagamentos = Pagamento.objects.all()
+    return render(request, 'lista_pagamento.html', {"pagamentos": pagamentos})
+
+""" def home_pagamento(request):
     
+    if request.method == 'POST':
+        form = PagamentoForm(request.POST)
+        if form.is_valid():
+            # Salvar os dados do formulário
+            form.save()
+            return redirect('/pagamento')
+    else:
+        form = PagamentoForm()
+    
+    return render(request, 'lista_pagamento.html', {'form': form})
+ """
+def pagamento_criar_view(request):
+
     if request.method == 'POST':
         form = PagamentoForm(request.POST)
         if form.is_valid():
