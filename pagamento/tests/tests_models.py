19ad6ec8381010ab414f8730ab524fa4baf1acae
hilariodantas <hilariod94@gmail.com>
2023-06-26 23:43:51 -0300

    testes #75

--- pagamento/tests/tests_models.py
+++ pagamento/tests/tests_models.py
@@ -1,1 +1,8 @@
 from django.test import TestCase
+from pagamento.models import Pagamento
+
+class PagamentoModelTest(TestCase):
+    def test_create_pagamento(self):
+        pagamento = Pagamento.objects.create(titulo='Pagamento Teste', valor=100)
+        self.assertEqual(pagamento.titulo, 'Pagamento Teste')
+        self.assertEqual(pagamento.valor, 100)
--- pagamento/tests/tests_views.py
+++ pagamento/tests/tests_views.py
@@ -1,1 +1,36 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from pagamento.models import Pagamento
+from pagamento.forms import PagamentoForm
+
+class PagamentoViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.list_url = reverse('home_pagamento_view')
+        self.create_url = reverse('criar_pagamento_view')
+        self.edit_url = reverse('editar_pagamento_view', args=[1])  # Supondo que o ID do pagamento seja 1
+        self.remove_url = reverse('remover_pagamento_view', args=[1])  # Supondo que o ID do pagamento seja 1
+
+    def test_home_pagamento_view(self):
+        response = self.client.get(self.list_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'lista_pagamento.html')
+
+    def test_criar_pagamento_view(self):
+        form_data = {'titulo': 'Pagamento Teste', 'valor': 100}
+        response = self.client.post(self.create_url, data=form_data)
+        self.assertEqual(response.status_code, 302)  # Verifica se houve redirecionamento após o POST
+        self.assertEqual(response.url, '/pagamento')
+
+    def test_editar_pagamento_view(self):
+        pagamento = Pagamento.objects.create(titulo='Pagamento Teste', valor=100)
+        form_data = {'titulo': 'Pagamento Editado', 'valor': 200}
+        response = self.client.post(self.edit_url, data=form_data)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, '/pagamento')
+
+    def test_remover_pagamento_view(self):
+        pagamento = Pagamento.objects.create(titulo='Pagamento Teste', valor=100)
+        response = self.client.post(self.remove_url)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, '/pagamento')
