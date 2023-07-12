232af040d8f5e10cb8321b3cc64b0ecc763f94f3
hilariodantas <hilariod94@gmail.com>
2023-07-12 11:00:49 -0300

    removendo models que não estava sendo usado #75

--- pagamento/models.py
+++ pagamento/models.py
@@ -1,20 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
-
-# Modelo Forma de Pagamento
-
-class FormaPagamento(models.Model):
-    nome = models.CharField(max_length=100)
-    descricao = models.TextField()
-    taxas = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
-
-    def __str__(self):
-        return self.nome
-    
-    
-# Modelo Pagamento
 class Pagamento(models.Model):
     STATUS_CHOICES = [
         ('pendente', 'Pendente'),
