62ff1c271b45e7f8fedce4490753bd89f5e59236
Adriel <adriel.fsantos@outlook.com>
2023-07-16 14:13:50 -0300

    Melhora DespesaForm #132

--- despesa/forms.py
+++ despesa/forms.py
@@ -3,10 +3,18 @@ from django import forms
 from despesa.models import Despesa
 
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+
 class DespesaForm(forms.ModelForm):
     class Meta:
         model = Despesa
-        fields = ['nome', 'valor', 'periodica']
+        fields = ['nome', 'valor', 'vencimento', 'periodica', 'paga']
+        widgets = {
+            'vencimento': DateInput(),
+            'valor': forms.NumberInput(attrs={'min': '0.01'})
+        }
         help_texts = {
             'periodica': "Marque caso essa despesa seja recorrente"
         }
