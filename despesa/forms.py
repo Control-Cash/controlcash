92f963de285da3216149410fa75fb44d0cc5d892
Adriel <adriel.fsantos@outlook.com>
2023-07-16 14:29:10 -0300

    Corrige formulário de despesa #132

--- despesa/forms.py
+++ despesa/forms.py
@@ -3,16 +3,12 @@ from django import forms
 from despesa.models import Despesa
 
 
-class DateInput(forms.DateInput):
-    input_type = 'date'
-
-
 class DespesaForm(forms.ModelForm):
     class Meta:
         model = Despesa
         fields = ['nome', 'valor', 'vencimento', 'periodica', 'paga']
         widgets = {
-            'vencimento': DateInput(),
+            'vencimento': forms.DateInput(attrs={'type': 'date'}, format='%Y-%m-%d'),
             'valor': forms.NumberInput(attrs={'min': '0.01'})
         }
         help_texts = {
