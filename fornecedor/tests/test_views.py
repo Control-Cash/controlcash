94d9bef0c9d46d45d3d49a15a8762c81e5033e78
Adriel <adriel.fsantos@outlook.com>
2023-07-17 16:02:42 -0300

    Adiciona testes para view editar fornecedor #139

--- fornecedor/tests/test_views.py
+++ fornecedor/tests/test_views.py
@@ -448,3 +448,237 @@ class DetalharFornecedorViewTest(TestCase):
             self.expected_template,
             'A view usou um template diferente do esperado'
         )
+
+
+class EditarFornecedorViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.expected_template = 'formAtualizaFornecedor.html'
+        self.endereco_criado = Endereco.objects.create(
+            rua='Rua do limoeiro',
+            bairro='Centro',
+            cidade='Sao Paulo',
+            estado='SP',
+            pais='Brasil',
+            cep='01001000',
+            numero=123,
+            complemento=''
+        )
+        self.fornecedor_criado = Fornecedor.objects.create(
+            nome='Americanas',
+            email='americanas@gmail.com',
+            endereco=self.endereco_criado
+        )
+        self.form_data = {
+            'nome': 'Magalu',
+            'email': 'magalu@outlook.com',
+            'rua': 'Rua do limoeiro',
+            'bairro': 'Centro',
+            'cidade': 'Sao Paulo',
+            'estado': 'SP',
+            'pais': 'Brasil',
+            'cep': '01001000',
+            'numero': '321',
+            'complemento': '',
+        }
+        self.target_url = reverse_lazy(
+            'fornecedor_atualizar',
+            kwargs={
+                'id': self.fornecedor_criado.id
+            }
+        )
+
+    def test_view_sends_form_to_template(self):
+        """Verifica se a view envia o formulário ao template"""
+
+        response = self.client.get(self.target_url)
+
+        self.assertIsNotNone(
+            response.context.get('form'),
+            "'form' não é enviado pela view"
+        )
+
+    def test_form_sended_to_template_is_correct_form(self):
+        """Verifica se a view envia o formulário correto ao template"""
+
+        response = self.client.get(self.target_url)
+
+        self.assertIsInstance(
+            response.context.get('form'),
+            CadastrarFornecedor,
+            "'form' enviado pela view não é o correto"
+        )
+        self.assertIsInstance(
+            response.context.get('endereco_form'),
+            EnderecoForm,
+            "'endereco_form' enviado pela view não é o correto"
+        )
+
+    def test_empty_form_doesnt_save(self):
+        """Verifica se a view não salva os dados quando um post request tem os dados em branco"""
+
+        self.client.post(self.target_url, {
+            'nome': '',
+            'email': '',
+            'rua': '',
+            'bairro': '',
+            'cidade': '',
+            'estado': '',
+            'pais': '',
+            'cep': '',
+            'numero': '',
+            'complemento': '',
+        })
+        fornecedor_atualizado = Fornecedor.objects.get(
+            id=self.fornecedor_criado.id)
+
+        self.assertEqual(
+            fornecedor_atualizado.nome,
+            self.fornecedor_criado.nome,
+            "O nome do fornecedor foi alterado com o formulário em branco"
+        )
+        self.assertEqual(
+            fornecedor_atualizado.email,
+            self.fornecedor_criado.email,
+            "O email do fornecedor foi alterado com o formulário em branco"
+        )
+
+    def test_filled_form_saves(self):
+        """Verifica se a view salva os dados quando um post request tem os dados corretos"""
+
+        self.client.post(self.target_url, self.form_data)
+        self.fornecedor_criado.refresh_from_db()
+
+        self.assertEqual(
+            self.fornecedor_criado.nome,
+            self.form_data.get('nome'),
+            "O nome do fornecedor não foi alterado com o formulário preenchido corretamente"
+        )
+        self.assertEqual(
+            self.fornecedor_criado.email,
+            self.form_data.get('email'),
+            "O email do fornecedor não foi alterado com o formulário preenchido corretamente"
+        )
+
+        self.assertEqual(
+            str(self.fornecedor_criado.endereco.numero),
+            self.form_data.get('numero'),
+            "O endereço do fornecedor não foi alterado com o formulário preenchido corretamente"
+        )
+
+    def test_view_uses_correct_template(self):
+        """Verifica se a view usa o template correto na renderização"""
+
+        response = self.client.get(self.target_url)
+
+        self.assertTemplateUsed(
+            response,
+            self.expected_template,
+            'A view usou um template diferente do esperado'
+        )
+
+    def test_view_returns_404_when_fornecedor_is_unknown(self):
+        """Verifica se a view retorna uma resposta 404 quando o cliente solicitado não existe"""
+
+        response = self.client.get(
+            reverse_lazy(
+                'fornecedor_atualizar',
+                kwargs={
+                    'id': 50
+                }
+            )
+        )
+
+        self.assertIsNone(response.context.get('form'))
+        self.assertEqual(response.status_code, 404)
+
+    def test_empty_required_fields_doesnt_save(self):
+        """Verifica se a view nao atualiza um fornecedor quando um post request tem os dados obrigatótios em branco"""
+
+        self.client.post(self.target_url, {
+            'nome': '',
+            'email': '',
+            'rua': 'Rua do limoeiro',
+            'bairro': 'Centro',
+            'cidade': '',
+            'estado': 'SP',
+            'pais': 'Brasil',
+            'cep': '01001000',
+            'numero': '321',
+            'complemento': '',
+        })
+        fornecedor_atualizado = Fornecedor.objects.get(
+            id=self.fornecedor_criado.id)
+
+        self.assertEqual(
+            fornecedor_atualizado.nome,
+            self.fornecedor_criado.nome,
+            "O fornecedor foi atualizado com os campos obrigatŕios do formulário em branco"
+        )
+
+    def test_empty_optional_fields_saves(self):
+        """Verifica se a view atualiza um fornecedor quando um post request tem os dados opcionais em branco"""
+
+        self.client.post(self.target_url, {
+            'nome': self.form_data.get('nome'),
+            'email': self.form_data.get('email'),
+            'rua': self.form_data.get('rua'),
+            'bairro': self.form_data.get('bairro'),
+            'cidade': self.form_data.get('cidade'),
+            'estado': self.form_data.get('estado'),
+            'pais': self.form_data.get('pais'),
+            'numero': self.form_data.get('numero'),
+            'cep': self.form_data.get('cep'),
+            'complemento': '',
+        }
+        )
+        fornecedor_atualizado = Fornecedor.objects.get(
+            id=self.fornecedor_criado.id)
+
+        self.assertEqual(
+            fornecedor_atualizado.nome,
+            self.form_data.get('nome'),
+            "Um fornecedor não foi atualizado com os campos opcionais do formulário em branco"
+        )
+
+    def test_empty_required_fields_return_errors(self):
+        """Verifica se a view envia erros ao template quando recebe todos os dados obrigatórios em branco"""
+
+        response = self.client.post(self.target_url, {
+            'nome': '',
+            'email': '',
+            'rua': '',
+            'bairro': '',
+            'cidade': '',
+            'estado': '',
+            'pais': '',
+            'cep': '',
+            'numero': '',
+            'complemento': 'lateral',
+        })
+        form = response.context.get('form')
+
+        self.assertIsNotNone(form.errors)
+
+    def test_redirects_to_correct_page_on_update(self):
+        """Verifica se a view redireciona para a url correta quando uma solicitação post é enviada corretamente com todos os dados"""
+
+        response = self.client.post(self.target_url, self.form_data)
+
+        self.assertRedirects(
+            response,
+            reverse_lazy('fornecedor_listar'),
+            302,
+            200
+        )
+
+    def test_form_loads_with_values_from_model(self):
+        """Verifica se a view envia o formulário com os campos preenchidos com os valores atuais salvos no banco de dados para o template"""
+
+        response = self.client.get(self.target_url)
+        form = response.context.get('form')
+
+        self.assertIsNotNone(form)
+        self.assertEqual(form.initial.get('nome'), self.fornecedor_criado.nome)
+        self.assertEqual(form.initial.get('email'),
+                         self.fornecedor_criado.email)
